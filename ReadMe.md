ea0789e9a0d425e98eb521d157b6a03bf0c4a550
Your Name <you@example.com>
2018-03-08 16:11:31 -0500

    redo

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+<h1> Robertson Brad Video Application </h1>
+
+<p> this is the video application using Node And Handlebars! </p>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <link href="css/main.css" rel="stylsheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Main Page</title>
+</head>
+<body>
+  <h1> Main page is here </h1>
+  <p> this is the main page of the site </p>
+
+</body>
+</html>
