b2a2bc4a315476254ddb8aea0bca26436b07f72a
Your Name <you@example.com>
2018-03-09 11:54:57 -0500

    update

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'express-handlebars');
-app.engine('handlebars', xhb ( defaultLayout : "index");
+app.set('view engine', 'handlebars');
+app.engine('handlebars', xhb({ defaultLayout : 'main'}));
 
 
 // uncomment after placing your favicon in /public
---
+++ confg.js
@@ -0,0 +1,11 @@
+var config = {
+  user : "root",
+  password : "", // leave this blank for windows machines
+  port: 8889,
+  host: "localhost",
+  database: "db_movies",
+
+  kidsmode : true
+}
+
+module.exports = config;
